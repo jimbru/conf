96f4cc9ad8d4a39ac67e34beeb175e8c455bb0fa
Jim Brusstar <jim.brusstar@gmail.com>
2016-06-22 22:48:30 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# conf
+
+A simple environment configuration library for Clojure.
+
+## Usage
+
+Add the following dependency to your `project.clj` file:
+```
+[conf "0.1.0-SNAPSHOT"]
+```
+
+## License
+
+Copyright © 2016 Jim Brusstar
+
+Distributed under the terms of the MIT License.
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject conf "0.1.0-SNAPSHOT"
+  :description "Simple configuration/environment library for Clojure."
+  :url "https://github.com/jimbru/conf"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
