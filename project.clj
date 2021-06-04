c917ecde6c59551e2978a55790928fba3090ba90
Michael Gardner <michael@treasuryprime.com>
2021-06-04 10:33:31 -0700

    Update Clojure to 1.10 (#6)
    * Update Clojure to `1.10.3`
    
    * Bump version to `0.13.0`

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject conf "0.12.0"
+(defproject conf "0.13.0"
   :description "Simple configuration/environment library for Clojure."
   :url "https://github.com/jimbru/conf"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.10.3"]])
